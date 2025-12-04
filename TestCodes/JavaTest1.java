771a20417d5634c3ef946e84372e3c2a6175a02c
anthoncalban <calbananthon@gmail.com>
2025-12-04 23:34:31 +0800

    Add files via upload

---
+++ TestCodes/JavaTest1.java
@@ -0,0 +1,19 @@
+int age = 21;
+int temperature = -15;
+byte maxByte = 100;
+short population = 15000;
+long distance = 500000L;
+long worldPopulation = 7900000000L;
+
+double height = 175.75;
+double bankBalance = 2500.00d;
+
+float width = 12.5f;
+float speed = 80;
+
+char initial = 'J';
+boolean isRaining = false;
+boolean isLoggedIn = true;
+
+String greeting = "Welcome to Java!";
+String quote = "She said, \"Keep going!\"";
